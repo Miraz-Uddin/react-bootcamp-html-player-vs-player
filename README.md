d7277defcf1fe37493633b3512fe39a9b1df25d0
Miraz Vai <freelancer.miraz.uddin@gmail.com>
2023-07-02 21:58:22 +0600

    Project Files Added

---
+++ README.md
@@ -0,0 +1,3 @@
+## Player vs Player
+
+Live link: https://player-vs-player-by-miraz.netlify.app
---
+++ assets/js/scripts.js
@@ -0,0 +1,5 @@
+(() => {
+  document.getElementById("year").innerHTML = new Date().getFullYear();
+  document.getElementById("company_name").innerHTML = "Md. Miraz Uddin";
+  // Write Your Code / Logic Below This Line
+})();
